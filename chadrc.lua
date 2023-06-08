2c0a7b175c52e1b7f7d19283958abe834523517a
Jérémy Dumas <57810034+j-dumas@users.noreply.github.com>
2023-06-08 14:54:36 -0400

    Update chadrc.lua

--- chadrc.lua
+++ chadrc.lua
@@ -12,7 +12,25 @@ M.ui = {
 
   nvdash = {
     load_on_startup = true,
+  },tabufline = {
+    enabled = false
   },
+  statusline = {
+    overriden_modules = function()
+      local st_modules = require "nvchad_ui.statusline.default"
+      -- this is just default table of statusline modules
+  
+      return {
+        fileInfo = function()
+          if (vim.bo[0].modified and "%") then
+            return st_modules.fileInfo():gsub('%#St_file_sep# ', '') .. " [+] " .. "%#St_file_sep#" .. " "
+          else
+            return st_modules.fileInfo()
+          end
+        end,
+      }
+    end,
+  }
 }
 
 M.plugins = "custom.plugins"
