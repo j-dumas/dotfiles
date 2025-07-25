178e8c5795b806cc017c264a03c757874d8b1f9f
jdumas <57810034+j-dumas@users.noreply.github.com>
2025-07-25 00:38:39 -0400

    Auto enabling LSPs with mason-lspconfig

--- nvim/.config/nvim/lua/configs/lspconfig.lua
+++ nvim/.config/nvim/lua/configs/lspconfig.lua
@@ -1,77 +1,43 @@
-require("nvchad.configs.lspconfig").defaults()
-local nvlsp = require("nvchad.configs.lspconfig")
-
-local servers = { "clangd", "ltex" }
-vim.lsp.enable(servers)
+local nvlsp = require "nvchad.configs.lspconfig"
+nvlsp.defaults()
 
 vim.lsp.config.clangd = {
-	cmd = { "clangd", "--enable-config" },
+  cmd = { "clangd", "--enable-config" },
+}
+
+vim.lsp.config.pylsp = {
+  settings = {
+    pylsp = {
+      plugins = {
+        pycodestyle = {
+          ignore = { "E203" },
+          maxLineLength = 88, -- Black formatter
+        },
+      },
+    },
+  },
 }
 
 -- Still use lspconfig to setup properly ltex_extra
 require("lspconfig").ltex.setup = {
-	on_init = nvlsp.on_init,
-	on_attach = function(client, bufnr)
-		nvlsp.on_attach(client, bufnr)
-		require("ltex_extra").setup({
-			load_langs = { "fr", "en-CA" },
-			path = vim.fn.expand("~") .. "/.local/share/ltex",
-		})
-	end,
-	capabilities = nvlsp.capabilities,
-	filetypes = { "bib", "markdown", "tex", "plaintex" },
-	settings = {
-		ltex = {
-			language = { "en-CA" },
-			additionalRules = {
-				motherTongue = { "fr" },
-				enablePickyRules = true,
-			},
-			enabled = { "bibtex", "markdown", "tex", "latex" },
-		},
-	},
+  on_init = nvlsp.on_init,
+  on_attach = function(client, bufnr)
+    nvlsp.on_attach(client, bufnr)
+    require("ltex_extra").setup {
+      load_langs = { "fr", "en-CA" },
+      path = vim.fn.expand "~" .. "/.local/share/ltex",
+    }
+  end,
+  capabilities = nvlsp.capabilities,
+  filetypes = { "bib", "markdown", "tex", "plaintex" },
+  settings = {
+    ltex = {
+      language = { "en-CA" },
+      additionalRules = {
+        motherTongue = { "fr" },
+        enablePickyRules = true,
+      },
+      enabled = { "bibtex", "markdown", "tex", "latex" },
+    },
+  },
 }
-
--- local lspconfig = require("lspconfig")
---
--- local masonlsp = require("mason-lspconfig")
---
--- masonlsp.setup()
--- masonlsp.setup_handlers({
---
--- 	function(lsp)
--- 		lspconfig[lsp].setup({
--- 			on_init = nvlsp.on_init,
--- 			on_attach = nvlsp.on_attach,
--- 			capabilities = nvlsp.capabilities,
--- 		})
--- 	end,
---
--- 	["clangd"] = function()
--- 		lspconfig["clangd"].setup({
--- 			on_init = nvlsp.on_init,
--- 			on_attach = nvlsp.on_attach,
--- 			capabilities = nvlsp.capabilities,
--- 			cmd = { "clangd", "--enable-config" },
--- 		})
--- 	end,
---
--- 	-- ["pylsp"] = function()
--- 	-- 	lspconfig["pylsp"].setup({
--- 	-- 		on_init = nvlsp.on_init,
--- 	-- 		on_attach = nvlsp.on_attach,
--- 	-- 		capabilities = nvlsp.capabilities,
--- 	-- 		settings = {
--- 	-- 			pylsp = {
--- 	-- 				plugins = {
--- 	-- 					pycodestyle = {
--- 	-- 						ignore = { "E203" },
--- 	-- 						maxLineLength = 88, -- Black formatter
--- 	-- 					},
--- 	-- 				},
--- 	-- 			},
--- 	-- 		},
--- 	-- 	})
--- 	-- end,
---
--- })
--- nvim/.config/nvim/lua/plugins/plugins.lua
+++ nvim/.config/nvim/lua/plugins/plugins.lua
@@ -1,127 +1,122 @@
-local overrides = require("configs.overrides")
+local overrides = require "configs.overrides"
 
 local nvchad = {
-	-- override plugin configs
-	{
-		"williamboman/mason.nvim",
-		opts = overrides.mason,
-	},
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    cmd = "Telescope",
+    opts = function()
+      local config = require "nvchad.configs.telescope"
+      config.pickers = { find_files = { hidden = true } }
+      return config
+    end,
+  },
 
-	{
-		"nvim-telescope/telescope.nvim",
-		dependencies = { "nvim-treesitter/nvim-treesitter" },
-		cmd = "Telescope",
-		opts = function()
-			local config = require("nvchad.configs.telescope")
-			config.pickers = { find_files = { hidden = true } }
-			return config
-		end,
-	},
+  -- 0.11 introduced vim.lsp.config making mason-lspconfig automatically enable lsp only
+  {
+    "mason-org/mason-lspconfig.nvim",
+    opts = {},
+    dependencies = {
+      "mason-org/mason.nvim",
+    },
+  },
 
-	-- 0.11 introduced vim.lsp.config
-	-- {
-	--   "williamboman/mason-lspconfig.nvim",
-	--   config = function()
-	--     require "configs.lspconfig"
-	--   end, -- Override to setup mason-lspconfig
-	--   dependencies = {
-	--     "williamboman/mason.nvim",
-	--   },
-	-- },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+    dependencies = {
+      "mason-org/mason-lspconfig.nvim",
+    },
+  },
 
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			require("configs.lspconfig")
-		end,
-	},
+  "mfussenegger/nvim-lint",
+  "rshkarin/mason-nvim-lint",
 
-	"mfussenegger/nvim-lint",
-	"rshkarin/mason-nvim-lint",
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = overrides.treesitter,
+  },
 
-	{
-		"nvim-treesitter/nvim-treesitter",
-		opts = overrides.treesitter,
-	},
+  {
+    "nvim-tree/nvim-tree.lua",
+    opts = overrides.nvimtree,
+  },
 
-	{
-		"nvim-tree/nvim-tree.lua",
-		opts = overrides.nvimtree,
-	},
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "micangl/cmp-vimtex",
+    },
+    opts = function()
+      local config = require "nvchad.configs.cmp"
+      table.insert(config.sources, {
+        name = "vimtex",
+      })
+      return config
+    end,
+  },
 
-	{
-		"hrsh7th/nvim-cmp",
-		dependencies = {
-			"micangl/cmp-vimtex",
-		},
-		opts = function()
-			local config = require("nvchad.configs.cmp")
-			table.insert(config.sources, {
-				name = "vimtex",
-			})
-			return config
-		end,
-	},
+  {
+    "max397574/better-escape.nvim",
+    event = "InsertEnter",
+    config = function()
+      require("better_escape").setup()
+    end,
+  },
 
-	{
-		"max397574/better-escape.nvim",
-		event = "InsertEnter",
-		config = function()
-			require("better_escape").setup()
-		end,
-	},
+  {
+    "christoomey/vim-tmux-navigator",
+    lazy = false,
+    enabled = function()
+      if jit then
+        return jit.os == "Linux"
+      else
+        return false
+      end
+    end,
+  },
 
-	{
-		"christoomey/vim-tmux-navigator",
-		lazy = false,
-		enabled = function()
-			if jit then
-				return jit.os == "Linux"
-			else
-				return false
-			end
-		end,
-	},
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup {
+        -- Configuration here, or leave empty to use defaults
+      }
+    end,
+  },
 
-	{
-		"kylechui/nvim-surround",
-		version = "*", -- Use for stability; omit to use `main` branch for the latest features
-		event = "VeryLazy",
-		config = function()
-			require("nvim-surround").setup({
-				-- Configuration here, or leave empty to use defaults
-			})
-		end,
-	},
+  -- Latex
+  {
+    "lervag/vimtex",
+    lazy = false, -- we don't want to lazy load VimTeX
+    ft = "tex",
+    init = function()
+      if jit then
+        if jit.os == "Linux" then
+          vim.g.vimtex_view_method = "zathura"
+        elseif jit.os == "Windows" then
+          vim.g.vimtex_view_general_viewer = "SumatraPDF"
+          vim.g.vimtex_view_general_options = "-reuse-instance -forward-search @tex @line @pdf"
+        end
+      end
 
-	-- Latex
-	{
-		"lervag/vimtex",
-		lazy = false, -- we don't want to lazy load VimTeX
-		ft = "tex",
-		init = function()
-			if jit then
-				if jit.os == "Linux" then
-					vim.g.vimtex_view_method = "zathura"
-				elseif jit.os == "Windows" then
-					vim.g.vimtex_view_general_viewer = 'SumatraPDF'
-					vim.g.vimtex_view_general_options = '-reuse-instance -forward-search @tex @line @pdf'
-				end
-			end
+      vim.g.vimtex_compiler_latexmk = {
+        aux_dir = "build",
+      }
+    end,
+  },
 
-			vim.g.vimtex_compiler_latexmk = {
-				aux_dir = 'build'
-			}
-		end,
-	},
+  { "micangl/cmp-vimtex", ft = "tex" },
 
-	{ "micangl/cmp-vimtex", ft = "tex" },
-
-	{
-		"barreiroleo/ltex_extra.nvim",
-		ft = { "markdown", "tex" },
-		dependencies = { "neovim/nvim-lspconfig" },
-	},
+  {
+    "barreiroleo/ltex_extra.nvim",
+    ft = { "markdown", "tex" },
+    dependencies = { "neovim/nvim-lspconfig" },
+  },
 }
 
 return nvchad
