11ca581e075742e1cb8258bee318bac1424161ae
j-dumas <57810034+j-dumas@users.noreply.github.com>
2025-09-19 22:53:17 -0400

    Corrected lspconfig for vim.lsp.config

--- nvim/.config/nvim/lua/configs/lspconfig.lua
+++ nvim/.config/nvim/lua/configs/lspconfig.lua
@@ -1,44 +1,54 @@
-local nvlsp = require "nvchad.configs.lspconfig"
+local nvlsp = require("nvchad.configs.lspconfig")
 nvlsp.defaults()
 
 vim.lsp.config.clangd = {
-  cmd = { "clangd", "--enable-config" },
+	cmd = { "clangd", "--enable-config" },
 }
 
 vim.lsp.config.pylsp = {
-  settings = {
-    pylsp = {
-      plugins = {
-        pycodestyle = {
-          ignore = { "E203" },
-          maxLineLength = 88, -- Black formatter
-        },
-        black = { enabled = true },
-      },
-    },
-  },
+	settings = {
+		pylsp = {
+			plugins = {
+				pycodestyle = {
+					ignore = { "E203" },
+					maxLineLength = 88, -- Black formatter
+				},
+				black = { enabled = true },
+			},
+		},
+	},
+}
+
+vim.lsp.config.lua_ls = {
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = { "vim" },
+			},
+		},
+	},
 }
 
 -- Still use lspconfig to setup properly ltex_extra
-require("lspconfig").ltex.setup = {
-  on_init = nvlsp.on_init,
-  on_attach = function(client, bufnr)
-    nvlsp.on_attach(client, bufnr)
-    require("ltex_extra").setup {
-      load_langs = { "fr", "en-CA" },
-      path = vim.fn.expand "~" .. "/.local/share/ltex",
-    }
-  end,
-  capabilities = nvlsp.capabilities,
-  filetypes = { "bib", "markdown", "tex", "plaintex" },
-  settings = {
-    ltex = {
-      language = { "en-CA" },
-      additionalRules = {
-        motherTongue = { "fr" },
-        enablePickyRules = true,
-      },
-      enabled = { "bibtex", "markdown", "tex", "latex" },
-    },
-  },
+vim.lsp.config.ltex = {
+	on_init = nvlsp.on_init,
+	on_attach = function(client, bufnr)
+		nvlsp.on_attach(client, bufnr)
+		require("ltex_extra").setup({
+			load_langs = { "fr", "en-CA" },
+			path = vim.fn.expand("~") .. "/.local/share/ltex",
+		})
+	end,
+	capabilities = nvlsp.capabilities,
+	filetypes = { "bib", "markdown", "tex", "plaintex" },
+	settings = {
+		ltex = {
+			language = { "en-CA" },
+			additionalRules = {
+				motherTongue = { "fr" },
+				enablePickyRules = true,
+			},
+			enabled = { "bibtex", "markdown", "tex", "latex" },
+		},
+	},
 }
