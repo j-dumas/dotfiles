0ec035d99c9a40669cd1c47b2a680d10a3b0a762
j-dumas <57810034+j-dumas@users.noreply.github.com>
2026-06-02 16:08:36 -0400

    Start of new nvim config

---
+++ nvim/.config/nvim/lua/options.lua
@@ -0,0 +1,38 @@
+require('vim._core.ui2').enable()
+
+vim.g.netrw_banner = 0
+vim.g.mapleader = ' '
+
+vim.o.nu = true
+vim.o.relativenumber = true
+
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+vim.o.smartindent = true
+
+vim.o.wrap = true
+vim.o.linebreak = true
+vim.o.breakindent = true
+
+vim.o.inccommand = "split"
+vim.o.splitbelow = true
+vim.o.splitright = true
+
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.laststatus = 3
+
+vim.o.swapfile = false
+vim.o.backup = false
+vim.o.undodir = vim.fn.stdpath("data") .. "/undodir"
+vim.o.undofile = true
+
+vim.o.clipboard = "unnamedplus"
+vim.o.cursorline = true
+vim.o.cursorlineopt = "number"
+vim.o.scrolloff = 8
+vim.o.signcolumn = "yes"
+vim.o.termguicolors = true
+vim.opt.whichwrap:append "<>[]hl"
---
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -0,0 +1,52 @@
+vim.pack.add { 
+    { 
+        src = "https://github.com/catppuccin/nvim",
+        name = "catppuccin" 
+    } ,
+    'https://github.com/nvim-tree/nvim-web-devicons',
+    'https://github.com/nvim-lualine/lualine.nvim'
+}
+
+require("catppuccin").setup({
+    flavour = "mocha",
+    transparent_background = true,
+    integrations = {
+        lualine = {
+            mocha = function(colors)
+                return {
+                    normal = {
+                        a = { bg = "#8bc2f0" },
+                        b = { fg = colors.text },
+                        c = { fg = colors.surface2 },
+                    },
+                    insert = {
+                        b = { fg = colors.text },
+                    },
+                    visual = {
+                        b = { fg = colors.text },
+                    },
+                    command = {
+                        b = { fg = colors.text },
+                    },
+                }
+            end
+        }
+    }
+})
+vim.cmd.colorscheme "catppuccin-nvim"
+
+require('lualine').setup({
+    options = {
+        component_separators = {left = '', right = ''},
+        section_separators = {left = '◤', right = ''}
+    },
+    sections = {
+        lualine_a = {'mode'},
+        lualine_b = {{'filename', path = 1}},
+        lualine_c = {'branch', 'diff'},
+        lualine_x = {'diagnostics', 'lsp_status'},
+        lualine_y = {'location'},
+        lualine_z = {'progress'}
+    },
+    extensions = {'nvim-tree'}
+})
