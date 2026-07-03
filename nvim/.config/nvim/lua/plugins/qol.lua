476d1630c4cdd8a2b080b3176a3c157061896179
j-dumas <57810034+j-dumas@users.noreply.github.com>
2026-07-02 22:45:51 -0400

    Added autopairs

--- nvim/.config/nvim/lua/plugins/qol.lua
+++ nvim/.config/nvim/lua/plugins/qol.lua
@@ -1,3 +1,7 @@
-vim.pack.add({ "https://github.com/folke/which-key.nvim" })
+vim.pack.add({
+    "https://github.com/folke/which-key.nvim",
+    "https://github.com/windwp/nvim-autopairs",
+})
 
 require("which-key").setup({ preset = "helix" })
+require("nvim-autopairs").setup({})
